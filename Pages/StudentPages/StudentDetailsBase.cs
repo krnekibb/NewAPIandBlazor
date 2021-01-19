c41546e3f0ef0237142644723c516a0ed418bd5b
krnekibb <bracunbostjan@gmail.com>
2021-01-19 21:12:38 +0100

    podatki preko enrollment

--- Pages/StudentPages/StudentDetailsBase.cs
+++ Pages/StudentPages/StudentDetailsBase.cs
@@ -17,7 +17,7 @@ namespace BlazorWebServer.Pages
         public IStudentService StudentService { get; set; }
 
         //public Student Student { get; set; }
-        //public List<Enrollment> Enrollments { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
 
         public ICollection<Student> Student { get; set; }
 
@@ -28,7 +28,7 @@ namespace BlazorWebServer.Pages
         {
             Id = Id ?? "1";
             //Student = await StudentService.GetStudent(int.Parse(Id));
-            //Enrollments = (await EnrollmentService.GetEnrollmentBySID(int.Parse(Id))).ToList();
+            Enrollments = (await EnrollmentService.GetEnrollmentBySID(int.Parse(Id))).ToList();
             Student = (await StudentService.GetStudent(int.Parse(Id))).ToList();
         }
     }
